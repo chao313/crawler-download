5e56092e5ddc4b2542b1c2d74c7801b10ba8825a
wanghaichao <m18888106873@163.com>
2020-12-31 08:16:01 +0800

    修改run.sh和start.sh

--- src/main/resources/docker/modelPro/init/run.sh
+++ src/main/resources/docker/modelPro/init/run.sh
@@ -20,48 +20,48 @@ fi
 
 sed -i -e "s/cfg\['blowfish_secret'\] = ''/cfg['blowfish_secret'] = '`date | md5sum`'/" /var/www/phpmyadmin/config.inc.php
 
-#mkdir -p /var/run/mysqld
-#
-#if [ -n "$VAGRANT_OSX_MODE" ];then
-#    usermod -u $DOCKER_USER_ID www-data
-#    groupmod -g $(($DOCKER_USER_GID + 10000)) $(getent group $DOCKER_USER_GID | cut -d: -f1)
-#    groupmod -g ${DOCKER_USER_GID} staff
-#    chmod -R 770 /var/lib/mysql
-#    chmod -R 770 /var/run/mysqld
-#    chown -R www-data:staff /var/lib/mysql
-#    chown -R www-data:staff /var/run/mysqld
-#else
-#    # Tweaks to give Apache/PHP write permissions to the app
-#    chown -R www-data:staff /var/www
-#    chown -R www-data:staff /app
-#    chown -R www-data:staff /var/lib/mysql
-#    chown -R www-data:staff /var/run/mysqld
-#    chmod -R 770 /var/lib/mysql
-#    chmod -R 770 /var/run/mysqld
-#fi
-#
-#rm /var/run/mysqld/mysqld.sock
-#
-#sed -i "s/bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
-#sed -i "s/user.*/user = www-data/" /etc/mysql/my.cnf
-#
-#if [[ ! -d $VOLUME_HOME/mysql ]]; then
-#    echo "=> An empty or uninitialized MySQL volume is detected in $VOLUME_HOME"
-#    echo "=> Installing MySQL ..."
-#
-#    # Try the 'preferred' solution
-#    mysqld --initialize-insecure > /dev/null 2>&1
-#
-#    # IF that didn't work
-#    if [ $? -ne 0 ]; then
-#        # Fall back to the 'depreciated' solution
-#        mysql_install_db > /dev/null 2>&1
-#    fi
-#
-#    echo "=> Done!"
-#    /create_mysql_users.sh
-#else
-#    echo "=> Using an existing volume of MySQL"
-#fi
+mkdir -p /var/run/mysqld
+
+if [ -n "$VAGRANT_OSX_MODE" ];then
+    usermod -u $DOCKER_USER_ID www-data
+    groupmod -g $(($DOCKER_USER_GID + 10000)) $(getent group $DOCKER_USER_GID | cut -d: -f1)
+    groupmod -g ${DOCKER_USER_GID} staff
+    chmod -R 770 /var/lib/mysql
+    chmod -R 770 /var/run/mysqld
+    chown -R www-data:staff /var/lib/mysql
+    chown -R www-data:staff /var/run/mysqld
+else
+    # Tweaks to give Apache/PHP write permissions to the app
+    chown -R www-data:staff /var/www
+    chown -R www-data:staff /app
+    chown -R www-data:staff /var/lib/mysql
+    chown -R www-data:staff /var/run/mysqld
+    chmod -R 770 /var/lib/mysql
+    chmod -R 770 /var/run/mysqld
+fi
+
+rm /var/run/mysqld/mysqld.sock
+
+sed -i "s/bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
+sed -i "s/user.*/user = www-data/" /etc/mysql/my.cnf
+
+if [[ ! -d $VOLUME_HOME/mysql ]]; then
+    echo "=> An empty or uninitialized MySQL volume is detected in $VOLUME_HOME"
+    echo "=> Installing MySQL ..."
+
+    # Try the 'preferred' solution
+    mysqld --initialize-insecure > /dev/null 2>&1
+
+    # IF that didn't work
+    if [ $? -ne 0 ]; then
+        # Fall back to the 'depreciated' solution
+        mysql_install_db > /dev/null 2>&1
+    fi
+
+    echo "=> Done!"
+    /create_mysql_users.sh
+else
+    echo "=> Using an existing volume of MySQL"
+fi
 
 exec supervisord -
--- src/main/resources/docker/modelPro/init/start.sh
+++ src/main/resources/docker/modelPro/init/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-echo '172.17.0.1 host.docker.internal' >> /etc/hosts
+#echo '172.17.0.1 host.docker.internal' >> /etc/hosts
 sh  /app/init/init.sh
 /run.sh
